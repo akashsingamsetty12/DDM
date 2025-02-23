406b1ba994235083c27bfde3f823843f5b3b1d25
akashsingamsetty12 <akashsingamsetty123@gmail.com>
2025-02-23 15:07:37 +0530

    first comm

--- src/components/Cinema.js
+++ src/components/Cinema.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Cinema.css";
-
+// import Aq from "../Images/DDMLOGO.jpg";
 const Cinema = ({ onClose }) => {
   return (
     <div className="cinema-overlay">
+
       <div className="cinema-content">
         <h2>Welcome to the DDM-01</h2>
         <p>Wait for a while and explore the Events</p>
